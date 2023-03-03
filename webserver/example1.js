bd3d57c4bd56f6729af77de2545fd0a0a5017f36
kennethz374 <kennethzhang374@gmail.com>
2023-03-03 15:03:51 -0800

    testing

--- webserver/example1.js
+++ webserver/example1.js
@@ -1,47 +1,47 @@
-callWs = function(){
-	// The Endpoint URL
-	let url = 'https://jsonplaceholder.typicode.com/posts/1';
-	fetch(url)
-  .then(function(response) {
-  	// Render the Response Status
-  	document.getElementById('result').innerHTML = response.status;
-    // Parse the body as JSON
-    return response.json();
-  })
-  .then(function(json) {
-  	// Render the parsed body
-  	document.getElementById('result_json').innerHTML = JSON.stringify(json);
-  })
-}
+callWs = function () {
+    // The Endpoint URL
+    let url = "https://jsonplaceholder.typicode.com/posts/1";
+    fetch(url)
+        .then(function (response) {
+            // Render the Response Status
+            document.getElementById("result").innerHTML = response.status;
+            // Parse the body as JSON
+            return response.json();
+        })
+        .then(function (json) {
+            // Render the parsed body
+            document.getElementById("result_json").innerHTML = JSON.stringify(json);
+        });
+};
 
-button2_function = function(){
-    // const url = 'http://10.195.100.158:5000/api/camera_settings';
-    const url = 'http://127.0.0.1:5000/api/camera_settings';
+button2_function = function () {
+    // const url = "http://10.195.100.158:5000/api/camera_settings";
+    const url = "http://127.0.0.1:5000/api/camera_settings";
 
-    console.log('url: ', url);
+    console.log("url: ", url);
 
-    const params = { 
-      model: 'BFS-U3-161S7M',
-      pixel_format: 'Mono8',
-      width: 5320,
-      height: 8,
-      isp: 'OFF',
-      adc: '8 Bit',
-      bin_selector: 'isp',
-      bin_x: 1,
-      bin_y: 1,
-      bin_mode_x: 'average',
-      bin_mode_y: 'average'
+    const params = {
+        model: "BFS-U3-161S7C",
+        pixel_format: "Mono8",
+        width: 5320,
+        height: 8,
+        isp: "OFF",
+        adc: "8 Bit",
+        bin_selector: "isp",
+        bin_x: 1,
+        bin_y: 1,
+        bin_mode_x: "average",
+        bin_mode_y: "average",
     };
-    
+
     const queryString = Object.entries(params)
-      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-      .join('&');
-    
+        .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+        .join("&");
+
     fetch(`${url}?${queryString}`)
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error(error));
 
     // const url = 'http://127.0.0.1:5000/';
     // console.log('url: ', url);
@@ -50,6 +50,4 @@ button2_function = function(){
     //   .then(response => response.json())
     //   .then(data => console.log(data))
     //   .catch(error => console.error(error));
-    
-}
-
+};
